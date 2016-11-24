9702597d1767052b9c1e2c2d7f9022b91b4d1923
Damien Tardy-Panis <damien@tardypad.me>
2016-11-24 18:05:43 +0100

    speed up ZSH startup (completion initialization hack)
    http://zsh.sourceforge.net/Doc/Release/Completion-System.html#Use-of-compinit
    https://gist.github.com/ctechols/ca1035271ad134841284
    Thanks @ctechols

--- zsh/zsh/completion.zsh
+++ zsh/zsh/completion.zsh
@@ -1,4 +1,11 @@
 autoload -Uz compinit
-compinit
+
+# Speed up startup by only checking once a day
+# if the cached .zcompdump file should be regenerated
+if [[ $( date +'%Y%j' ) > $( date +'%Y%j' -r ~/.zcompdump ) ]]; then
+  compinit
+else
+  compinit -C
+fi
 
 source /usr/local/etc/zsh_completion.d/git-extras-completion.zsh
