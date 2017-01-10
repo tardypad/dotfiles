c4114364dcf34fadcf486ef0d943c26826889a33
Damien Tardy-Panis <damien@tardypad.me>
2017-01-10 18:04:50 +0100

    add tmux plugins manager

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -3,4 +3,8 @@
 setup_tmux() {
   # copy config files
   copy "tmux/tmux.conf" ".tmux.conf"
+
+  # install the plugins manager if not present
+  [[ -d ~/.tmux/plugins/tpm ]] \
+    || git clone --quiet git@github.com:tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
