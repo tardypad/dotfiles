aec84ab4bb8b75ddc995217898382429a963f07c
Damien Tardy-Panis <damien@tardypad.me>
2017-01-17 18:23:00 +0100

    Disable hosts completion for ssh and scp from /etc/hosts and ~/.ssh/known_hosts
    only use completion of aliases from ~/.ssh/config
    better fix than previous commit ("reverted")

--- zsh/zsh/completion.zsh
+++ zsh/zsh/completion.zsh
@@ -18,3 +18,9 @@ source /usr/local/etc/zsh_completion.d/git-extras-completion.zsh
 # lowercase letters match lowercase and uppercase letters
 # uppercase letters match only uppercase letters
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+
+# Disable hosts completion for ssh an scp
+# from /etc/hosts and ~/.ssh/known_hosts
+# so that completion only reads aliases from ~/.ssh/config
+zstyle -e ':completion:*:*:(scp|ssh):*:*' hosts 'reply=()'
+zstyle -e ':completion:*:*:(scp|ssh):*:*' known-hosts-files 'reply=()'
