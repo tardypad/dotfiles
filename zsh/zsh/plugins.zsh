24500138c5451c02fdf751d7d59047802cefb8e6
Damien Tardy-Panis <damien@tardypad.me>
2017-02-10 13:08:53 +0100

    add solarized dircolors as zsh plugin
    Thanks @seebi

--- zsh/zsh/plugins.zsh
+++ zsh/zsh/plugins.zsh
@@ -1,4 +1,7 @@
 # load plugins manager
 source ~/.zplug/init.zsh
 
-
+# a bit abusive to put this one here (no ZSH thing)
+# but at least we can manage updates easily
+zplug 'seebi/dircolors-solarized', \
+  hook-build: "ln -s ${ZPLUG_REPOS}/seebi/dircolors-solarized/dircolors.ansi-dark ~/.dircolors"
