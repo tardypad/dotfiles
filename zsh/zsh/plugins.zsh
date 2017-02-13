5dfd7b2b94fd1ed0429779efec57d68784371f1c
Damien Tardy-Panis <damien@tardypad.me>
2017-02-13 17:56:06 +0100

    add revolver progress spinner for scripts as zsh plugin
    Thanks @molovo

--- zsh/zsh/plugins.zsh
+++ zsh/zsh/plugins.zsh
@@ -5,3 +5,11 @@ source ~/.zplug/init.zsh
 # but at least we can manage updates easily
 zplug 'seebi/dircolors-solarized', \
   hook-build: "ln -s ${ZPLUG_REPOS}/seebi/dircolors-solarized/dircolors.ansi-dark ~/.dircolors"
+
+# a progress spinner for ZSH scripts
+zplug "molovo/revolver", \
+  as:command, \
+  use:revolver
+
+# source plugins and add commands to PATH
+zplug load
