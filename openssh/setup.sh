741c682f137f9162da0bb13cac508452bb1b0b43
Damien Tardy-Panis <damien@tardypad.me>
2017-02-16 10:01:48 +0100

    rename ssh to openssh to be more explicit

--- openssh/setup.sh
+++ openssh/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 
-local_setup_ssh() {
-  cat "ssh/config" "ssh/config.local" \
+local_setup_openssh() {
+  cat "openssh/config" "openssh/config.local" \
     > "${HOME}/.ssh/config" 2>/dev/null
 
   chmod 600 "${HOME}/.ssh/config"
