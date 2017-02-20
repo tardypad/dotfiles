b852a8e08b76397547acf719fb0f0e1cd3c46911
Damien Tardy-Panis <damien@tardypad.me>
2017-02-20 13:20:32 +0100

    try to fix the display of the last tmux session name
    happened often that the last session name is not ready for display

--- tmux/tmux/scripts/cache_sessions_name.sh
+++ tmux/tmux/scripts/cache_sessions_name.sh
@@ -2,3 +2,7 @@
 
 tmux list-sessions -F "#{session_name}" \
   | xargs -I {} tmux run -b '#(echo {} | sed "s/^[0-9]*-//")'
+
+# wait a bit to make sure the last result gets processed fully
+# so that it can get directly displayed
+sleep 0.05
