823ada47ef5044d07dc346226a162422ca32649d
Damien Tardy-Panis <damien@tardypad.me>
2017-02-23 12:47:52 +0100

    openssh setup create .ssh folder with permissions

--- openssh/setup.sh
+++ openssh/setup.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/zsh
 
 local_setup_openssh() {
+  # create necessary directories
+  mkdir -p ~/.ssh
+
+  # copy config files
   cat "openssh/config" "openssh/config.local" \
     > "${HOME}/.ssh/config" 2>/dev/null
 
+  # adjust permissions
+  chmod 700 "${HOME}/.ssh"
   chmod 600 "${HOME}/.ssh/config"
 }
