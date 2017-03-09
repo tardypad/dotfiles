049a6ea9ea7b729d705bf7c8643ef32d39ebc29e
Damien Tardy-Panis <damien@tardypad.me>
2017-03-09 18:09:18 +0100

    add basic remote openssh setup

--- openssh/setup.sh
+++ openssh/setup.sh
@@ -12,3 +12,18 @@ openssh::local::setup() {
   chmod 700 "${HOME}/.ssh"
   chmod 600 "${HOME}/.ssh/config"
 }
+
+
+openssh::remote::setup() {
+  local host="$1"
+
+  # create necessary directories
+  ssh "${host}" 'mkdir -p ~/.ssh'
+
+  # copy config files
+  remote_copy "${host}" "openssh/config_${host}" ".ssh/config"
+
+  # adjust permissions
+  ssh "${host}" 'chmod 700 "${HOME}/.ssh"'
+  ssh "${host}" 'chmod 600 "${HOME}/.ssh/config"'
+}
