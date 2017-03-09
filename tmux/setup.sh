78e982e57b4b738b1bacae604ab9543a1ca49f6d
Damien Tardy-Panis <damien@tardypad.me>
2017-03-09 11:49:30 +0100

    add generic function to remote setup tmux

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -20,3 +20,27 @@ local_setup_tmux() {
   ~/.tmux/plugins/tpm/bin/install_plugins \
     > /dev/null 2>&1
 }
+
+
+remote_setup_tmux_generic() {
+  local host="$1"
+
+  # create necessary directories
+  ssh "${host}" '[[ -d ~/.tmux ]] || mkdir ~/.tmux'
+
+  # copy config files
+  remote_copy "${host}" "tmux/tmux.conf" ".tmux.conf"
+  remote_copy "${host}" "tmux/tmux/scripts" ".tmux/scripts"
+  remote_copy "${host}" "tmux/tmux/plugins.tmux" ".tmux/plugins.tmux"
+  remote_copy "${host}" "tmux/tmux/conf" ".tmux/conf"
+  remote_copy "${host}" "tmux/tmux/images" ".tmux/images"
+
+  # install the plugins manager if not present
+  ssh "${host}" '[[ -d ~/.tmux/plugins/tpm ]] \
+    || git clone --quiet https://github.com/tmux-plugins/tpm \
+       ~/.tmux/plugins/tpm'
+
+  # install the configured plugins
+  ssh "${host}" '~/.tmux/plugins/tpm/bin/install_plugins \
+    > /dev/null 2>&1'
+}
