5c28ac8984b57dc0773bfacffc2f127c8664a079
Damien Tardy-Panis <damien@tardypad.me>
2017-04-19 16:42:19 +0200

    tmux_env defines initial path, split, layout and command

--- zsh/zsh/scripts/tmux_env/local/local.sh
+++ zsh/zsh/scripts/tmux_env/local/local.sh
@@ -1,3 +1,32 @@
-tmux new-session -d -s '1-Local' -n 'root'
-tmux new-window -d -n 'dotfiles'
-tmux new-window -d -n 'desktop'
+session_name='1-Local'
+
+desktop_path=/home/damien/Desktop
+dotfiles_path=/home/damien/Projects/tardypad/dotfiles
+
+
+window_name='root'
+tmux new-session -d \
+  -s "${session_name}" \
+  -n "${window_name}" \
+  -c "${desktop_path}"
+
+
+window_name='dotfiles'
+tmux new-window -d \
+  -t "${session_name}" \
+  -n "${window_name}" \
+  -c "${dotfiles_path}"
+tmux split-window -h \
+  -t "${session_name}:${window_name}" \
+  -c "${dotfiles_path}" \
+  vim
+tmux select-layout \
+  -t "${session_name}:${window_name}" \
+  even-horizontal
+
+
+window_name='desktop'
+tmux new-window -d \
+  -t "${session_name}" \
+  -n "${window_name}" \
+  -c "${desktop_path}"
