aeddf11e60d23197124f2ea67fa6d85a5a997736
Damien Tardy-Panis <damien@tardypad.me>
2017-04-20 16:38:50 +0200

    use commands sequence in tmux env creation of windows
    prevent some weird empty lines display in splitted windows
    (make sure commands are run sequentially)

--- zsh/zsh/scripts/tmux_env/local/local.sh
+++ zsh/zsh/scripts/tmux_env/local/local.sh
@@ -1,28 +1,24 @@
-session_name='1-Local'
-
 desktop_path=/home/damien/Desktop
 dotfiles_path=/home/damien/Projects/tardypad/dotfiles
 
 
-window_name='root'
 tmux new-session -d \
-  -s "${session_name}" \
-  -n "${window_name}" \
+  -s '1-Local' \
+  -n 'root' \
   -c "${desktop_path}"
 
 
-window_name='dotfiles'
 tmux new-window \
-  -n "${window_name}" \
-  -c "${dotfiles_path}"
-tmux split-window -h \
+  -n 'dotfiles' \
   -c "${dotfiles_path}" \
-  vim
-tmux select-layout \
-  even-horizontal
+  \; \
+  split-window -h \
+  -c "${dotfiles_path}" \
+  vim \
+  \; \
+  select-layout even-horizontal
 
 
-window_name='desktop'
 tmux new-window \
-  -n "${window_name}" \
+  -n 'desktop' \
   -c "${desktop_path}"
