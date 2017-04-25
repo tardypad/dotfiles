c411e21fb43e9564afce84b04621e8d7e936ad87
Damien Tardy-Panis <damien@tardypad.me>
2017-04-25 16:52:56 +0200

    limit konsole env to 80 characters

--- zsh/zsh/scripts/konsole_env/local.sh
+++ zsh/zsh/scripts/konsole_env/local.sh
@@ -1,7 +1,13 @@
 qdbus_service="$1"
 session="$2"
 
-qdbus "${qdbus_service}" "/Sessions/${session}" org.kde.konsole.Session.setTabTitleFormat 0 '1:Local' &> /dev/null
-qdbus "${qdbus_service}" "/Sessions/${session}" org.kde.konsole.Session.setTabTitleFormat 1 '1:Local' &> /dev/null
+qdbus "${qdbus_service}" "/Sessions/${session}" \
+  org.kde.konsole.Session.setTabTitleFormat 0 '1:Local' \
+  &> /dev/null
+qdbus "${qdbus_service}" "/Sessions/${session}" \
+  org.kde.konsole.Session.setTabTitleFormat 1 '1:Local' \
+  &> /dev/null
 
-qdbus "${qdbus_service}" "/Sessions/${session}" org.kde.konsole.Session.runCommand 'start_tmux_env local' &> /dev/null
+qdbus "${qdbus_service}" "/Sessions/${session}" \
+  org.kde.konsole.Session.runCommand 'start_tmux_env local' \
+  &> /dev/null
