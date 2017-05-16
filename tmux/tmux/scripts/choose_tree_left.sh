5906c02b0f128778de38f0b01e6441ab2b044424
Damien Tardy-Panis <damien@tardypad.me>
2017-05-16 16:47:21 +0200

    prevent to have multiple tmux choice tree menus in the same window
    needed to use sequence of tmux commands
    otherwise it was then crashing

--- tmux/tmux/scripts/choose_tree_left.sh
+++ tmux/tmux/scripts/choose_tree_left.sh
@@ -4,22 +4,38 @@ menu_width=40
 
 type="$1"
 
+# abort if there is already a choice tree in the current window
+tmux list-panes -F '#{pane_start_command}' \
+  | grep -qE 'choose_window|choose_session'
+if [[ $? -eq 0 ]]; then
+  exit 0
+fi
+
+
 # use pane index instead of id
 # otherwise the restoring of pane seems to fail in case of id starting with %1
 # ¯\_(ツ)_/¯
 current_pane_index=$( tmux display -p '#{pane_index}' )
 current_window_layout=$( tmux display -p '#{window_visible_layout}' )
 
-# create new active menu pane
-# full window height, on most left part, with fixed width
-tmux select-pane -t :.left
-tmux split-window -hbf
-tmux resize-pane -x "${menu_width}"
-
 case "${type}" in
-  windows) select_command="select-window -t '%%'" ;;
-  sessions) select_command="switch-client -t '%%'" ;;
+  windows)
+    select_command="select-window -t '%%'"
+    pane_title='choose_window'
+    ;;
+  sessions)
+    select_command="switch-client -t '%%'"
+    pane_title='choose_session'
+    ;;
 esac
+
+# create new active menu pane
+# with an unexisting command just to set the pane title
+# full window height, on most left part, with fixed width
+tmux \
+  select-pane -t :.left \; \
+  split-window -hbf "${pane_title}" \; \
+  resize-pane -x "${menu_width}"
 
 # before switching sessions/windows
 # close menu, restore layout and active pane
