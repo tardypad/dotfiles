deb7fa43fde5223e5066d36b987b049e6c912b95
Damien Tardy-Panis <damien@tardypad.me>
2017-05-30 12:15:59 +0200

    add konsole setup to install Source Code Pro font

---
+++ konsole/setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/zsh
+
+konsole::local::setup() {
+  # create necessary directories
+  mkdir -p ~/.fonts
+
+  # install Adobe Source Code Pro font if not present
+  if [[ ! -d ~/.fonts/source-code-pro/ ]]; then
+    curl --silent --location \
+      https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.tar.gz \
+        | tar xz --strip-component=1 -C ~/.fonts/ source-code-pro-2.030R-ro-1.050R-it/OTF
+    mv ~/.fonts/OTF ~/.fonts/source-code-pro
+    fc-cache -f
+  fi
+}
