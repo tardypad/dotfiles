820f20a0e42e50408633bbf0bd902febd798d84c
Damien Tardy-Panis <damien@tardypad.me>
2017-07-24 12:01:40 +0200

    fix mycli pager setting conflict with mysqldump
    https://github.com/dbcli/mycli/issues/281
    mycli reads by default only the [client] section from my.cnf
    mysqldump reads it too and fails on unknown variable, like pager...
    pager is now defined in a mycli specific section

--- zsh/zsh/conf/alias.zsh
+++ zsh/zsh/conf/alias.zsh
@@ -16,3 +16,5 @@ alias PJ='goto_project_main'
 # lxc-console uses different prefix for escape command
 # (default Ctrl-a), not to conflict with tmux's one
 alias lxc-console="lxc-console --escape='^q'"
+
+alias mycli='mycli --defaults-group-suffix=_mycli'
