679bef0bd991f5fb58887ce2f0b41200e1d011c2
Damien Tardy-Panis <damien@tardypad.me>
2017-08-09 14:55:05 +0200

    add git global ignore files
    small redefinition of ignored files and folders
    keep in repos info/exclude only items most likely to be existing/used
    so as to be able to easily remove them

--- git/setup.sh
+++ git/setup.sh
@@ -7,5 +7,6 @@ git::local::setup() {
   # copy config files
   local_copy "git/git/config" ".config/git/config"
   local_copy "git/git/config.local" ".config/git/config.local"
+  local_copy "git/git/ignore" ".config/git/ignore"
   local_copy "git/git/template" ".config/git/template"
 }
