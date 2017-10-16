2ce0c40e07beefbca4f7a61977cc3a6b513c5542
Damien Tardy-Panis <damien@tardypad.me>
2017-10-16 14:58:44 +0200

    change konsole fonts location
    ~/.fonts/ is deprecated
    fix -C argument quoting problem during extracting

--- konsole/setup.sh
+++ konsole/setup.sh
@@ -2,10 +2,10 @@
 
 konsole::local::setup() {
   # create necessary directories
-  mkdir -p "${HOME}/.fonts"
   mkdir -p "${HOME}/.config"
   mkdir -p "${HOME}/.local/share/konsole"
   mkdir -p "${HOME}/.local/share/kxmlgui5/konsole"
+  mkdir -p "${HOME}/.local/share/fonts"
 
   # copy config files
   local_copy "konsole/konsolerc" ".config/konsolerc"
@@ -13,11 +13,11 @@ konsole::local::setup() {
   local_copy "konsole/kxmlgui5" ".local/share/kxmlgui5/konsole"
 
   # install Adobe Source Code Pro font if not present
-  if [[ ! -d "${HOME}/.fonts/source-code-pro/" ]]; then
+  if [[ ! -d "${HOME}/.local/share/fonts/source-code-pro/" ]]; then
     curl --silent --location \
       https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.tar.gz \
-        | tar xz --strip-component=1 -C "${HOME}/.fonts/ source-code-pro-2.030R-ro-1.050R-it/OTF"
-    mv "${HOME}/.fonts/OTF" "${HOME}/.fonts/source-code-pro"
+        | tar xz --strip-component=1 -C "${HOME}/.local/share/fonts" source-code-pro-2.030R-ro-1.050R-it/OTF
+    mv "${HOME}/.local/share/fonts/OTF" "${HOME}/.local/share/fonts/source-code-pro"
     fc-cache -f
   fi
 }
