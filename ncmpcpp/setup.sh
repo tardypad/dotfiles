251989a0f0ab574ca434e630dc22b51d83830d1e
Damien Tardy-Panis <damien@tardypad.me>
2017-10-17 15:12:21 +0200

    store ncmpcpp files in XDG folders

--- ncmpcpp/setup.sh
+++ ncmpcpp/setup.sh
@@ -2,9 +2,10 @@
 
 ncmpcpp::local::setup() {
   # create necessary directories
-  mkdir -p "${HOME}/.ncmpcpp"
+  mkdir -p "${CONFIG_DIR}/ncmpcpp"
+  mkdir -p "${DATA_DIR}/ncmpcpp"
 
   # copy config files
-  local_copy "ncmpcpp/config" "${HOME}/.ncmpcpp/config"
-  local_copy "ncmpcpp/bindings" "${HOME}/.ncmpcpp/bindings"
+  local_copy "ncmpcpp/config" "${CONFIG_DIR}/ncmpcpp/config"
+  local_copy "ncmpcpp/bindings" "${CONFIG_DIR}/ncmpcpp/bindings"
 }
