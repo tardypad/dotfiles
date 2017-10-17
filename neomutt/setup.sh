f6da9ae12e1bb42f2352ce1e192c4b4aa97054a9
Damien Tardy-Panis <damien@tardypad.me>
2017-10-17 15:42:14 +0200

    store neomutt files in XDG folders
    using neomutt / neomuttrc names doesn't seem to work though...

--- neomutt/setup.sh
+++ neomutt/setup.sh
@@ -2,9 +2,10 @@
 
 neomutt::local::setup() {
   # create necessary directories
-  mkdir -p "${HOME}/.mutt/"{cache/headers,cache/bodies,tmp}
+  mkdir -p "${CONFIG_DIR}/mutt"
+  mkdir -p "${CACHE_DIR}/mutt/"{headers,bodies,tmp}
 
   # copy config files
-  local_copy "neomutt/muttrc" "${HOME}/.mutt/muttrc"
-  local_copy "neomutt/conf" "${HOME}/.mutt/conf"
+  local_copy "neomutt/muttrc" "${CONFIG_DIR}/mutt/neomuttrc"
+  local_copy "neomutt/conf" "${CONFIG_DIR}/mutt/conf"
 }
