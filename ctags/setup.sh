4df0a6e057d148c084a2cd50ac20597acb5fc5b4
Damien Tardy-Panis <damien@tardypad.me>
2017-10-25 17:11:44 +0200

    new config files loading location and schema for ctags

--- ctags/setup.sh
+++ ctags/setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 
 ctags::local::setup() {
+  # create necessary directories
+  mkdir -p "${HOME}/.ctags.d"
+
   # copy config file
-  local_copy "ctags/ctags" "${HOME}/.ctags"
+  local_copy "ctags/main.ctags" "${HOME}/.ctags.d/main.ctags"
 }
