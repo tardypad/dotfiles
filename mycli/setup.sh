5857395df0d6528156c2602b5d4840e95e914aa4
Damien Tardy-Panis <damien@tardypad.me>
2017-11-29 11:49:14 +0100

    add mycli_env function to launch mycli with preset options
    similar to http_prompt_env
    
    couldn't use mycli arguments unfortunately
    (such as --login-path or -defaults-file)
    as they don't allow defining the database to use
    so an env just contains the command line options with values

--- mycli/setup.sh
+++ mycli/setup.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env zsh
 
 mycli::local::setup() {
+  # create necessary directories
+  mkdir -p "${DATA_DIR}/mycli/env"
+
   # copy config files
   local_copy "mycli/myclirc" "${HOME}/.myclirc"
   local_copy "mycli/my.cnf" "${HOME}/.my.cnf"
+  local_copy "mycli/env" "${DATA_DIR}/mycli/env"
 }
