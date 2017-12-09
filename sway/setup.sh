827805efd9d1e5cd5b69442d5f97e6851eb716fb
Damien Tardy-Panis <damien@tardypad.me>
2017-12-09 20:39:45 +0100

    set sway wallpaper
    coming from https://wallpaperscraft.com/download/leaf_drop_dew_surface_90302/2560x1440

--- sway/setup.sh
+++ sway/setup.sh
@@ -3,7 +3,9 @@
 sway::local::setup() {
   # create necessary directories
   mkdir -p "${CONFIG_DIR}/sway"
+  mkdir -p "${DATA_DIR}/sway"
 
   # copy config files
   local_copy "sway/config" "${CONFIG_DIR}/sway/config"
+  local_copy "sway/wallpaper.jpg" "${DATA_DIR}/sway/wallpaper.jpg"
 }
