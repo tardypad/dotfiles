5e1bd95d00cb9c09e2d468719f0ef897fad55dff
Damien Tardy-Panis <damien@tardypad.me>
2017-12-14 23:15:22 +0100

    automatically start sway after login

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -7,6 +7,7 @@ zsh::local::setup() {
   # copy config files
   local_copy "zsh/zshrc" "${HOME}/.zshrc"
   local_copy "zsh/zshenv" "${HOME}/.zshenv"
+  local_copy "zsh/zlogin" "${HOME}/.zlogin"
   local_copy "zsh/dircolors" "${HOME}/.dircolors"
   local_copy "zsh/zsh/conf" "${HOME}/.zsh/conf"
   local_copy "zsh/zsh/scripts" "${HOME}/.zsh/scripts"
