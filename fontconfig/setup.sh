97e7d316bb74066b15a60a8e1f9cee2479e05484
Damien Tardy-Panis <damien@tardypad.me>
2017-12-15 14:51:40 +0100

    move font download and setup from konsole to fontconfig tool

--- fontconfig/setup.sh
+++ fontconfig/setup.sh
@@ -3,7 +3,17 @@
 fontconfig::setup() {
   # create necessary directories
   mkdir -p "${CONFIG_DIR}/fontconfig"
+  mkdir -p "${DATA_DIR}/fonts"
 
   # copy config files
   copy "fontconfig/fonts.conf" "${CONFIG_DIR}/fontconfig/fonts.conf"
+
+  # install Adobe Source Code Pro font if not present
+  if [[ ! -d "${DATA_DIR}/fonts/source-code-pro/" ]]; then
+    curl --silent --location \
+      https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.tar.gz \
+        | tar xz --strip-component=1 -C "${DATA_DIR}/fonts" source-code-pro-2.030R-ro-1.050R-it/OTF
+    mv "${DATA_DIR}/fonts/OTF" "${DATA_DIR}/fonts/source-code-pro"
+    fc-cache -f
+  fi
 }
--- konsole/setup.sh
+++ konsole/setup.sh
@@ -5,19 +5,9 @@ konsole::setup() {
   mkdir -p "${CONFIG_DIR}"
   mkdir -p "${DATA_DIR}/konsole"
   mkdir -p "${DATA_DIR}/kxmlgui5/konsole"
-  mkdir -p "${DATA_DIR}/fonts"
 
   # copy config files
   copy "konsole/konsolerc" "${CONFIG_DIR}/konsolerc"
   copy "konsole/konsole" "${DATA_DIR}/konsole"
   copy "konsole/kxmlgui5" "${DATA_DIR}/kxmlgui5/konsole"
-
-  # install Adobe Source Code Pro font if not present
-  if [[ ! -d "${DATA_DIR}/fonts/source-code-pro/" ]]; then
-    curl --silent --location \
-      https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.tar.gz \
-        | tar xz --strip-component=1 -C "${DATA_DIR}/fonts" source-code-pro-2.030R-ro-1.050R-it/OTF
-    mv "${DATA_DIR}/fonts/OTF" "${DATA_DIR}/fonts/source-code-pro"
-    fc-cache -f
-  fi
 }
