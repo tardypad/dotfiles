7b935144af2253781c6fa4db6ae21ec073693890
Damien Tardy-Panis <damien@tardypad.me>
2017-12-26 17:14:33 +0100

    define build directory in XDG

--- pacman/setup.sh
+++ pacman/setup.sh
@@ -3,7 +3,7 @@
 pacman::setup() {
   # create necessary directories
   mkdir -p "${CONFIG_DIR}/pacman"
-  mkdir -p "${HOME}/build/"{packages,sources,srcpackages,makepkglogs}
+  mkdir -p "$( xdg-user-dir BUILD )/"{packages,sources,srcpackages,makepkglogs}
 
   # copy config files
   copy "pacman/makepkg.conf" "${CONFIG_DIR}/pacman/makepkg.conf"
