5aa3d6360ae6f52881fdb743d41784344ae5150d
Damien Tardy-Panis <damien@tardypad.me>
2018-01-16 17:31:27 +0100

    replace new tools setup scripts with Makefile
    follow change in master

--- login/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-login::setup() {
-  # copy config files
-  copy "login/hushlogin" "${HOME}/.hushlogin"
-}
--- termite/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-termite::setup() {
-  # create necessary directories
-  mkdir -p "${CONFIG_DIR}/termite"
-
-  # copy config files
-  copy "termite/config" "${CONFIG_DIR}/termite/config"
-}
