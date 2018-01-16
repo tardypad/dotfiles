726d64724adf54203466c39c85348fdb75a51cc5
Damien Tardy-Panis <damien@tardypad.me>
2018-01-16 15:52:44 +0100

    solarized fzf colors

--- zsh/zsh/conf/export.zsh
+++ zsh/zsh/conf/export.zsh
@@ -16,4 +16,9 @@ export READNULLCMD=less
 export KEYTIMEOUT=5
 
 # fzf config
-export FZF_DEFAULT_OPTS="--reverse --height=20"
+export FZF_DEFAULT_OPTS="
+--reverse
+--height=20
+--no-bold
+--color=bg:8,fg:12,hl:3,fg+:8,bg+:3,info:4,prompt:4,pointer:8,marker:7,spinner:4
+"
