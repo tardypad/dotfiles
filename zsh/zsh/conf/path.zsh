b724c2eed2eee06e874870900a70ab188d57acc7
Damien Tardy-Panis <damien@tardypad.me>
2018-02-10 12:25:06 +0100

    remove LD_LIBRARY_PATH environment variable
    is not needed anymore for now

--- zsh/zsh/conf/path.zsh
+++ zsh/zsh/conf/path.zsh
@@ -3,8 +3,5 @@ PATH="$HOME/.zsh/scripts:$PATH"
 PATH="$HOME/.config/sway/scripts:$PATH"
 export PATH
 
-# add local shared libraries
-export LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
-
 # set PATH in systemd user environment
 systemctl --user import-environment PATH
