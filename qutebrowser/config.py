6674722a16a05158ec13fa2573f0e2adead95560
Damien Tardy-Panis <damien@tardypad.me>
2018-03-07 17:13:39 +0100

    add google search engine to qutebrowser

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -12,3 +12,9 @@ c.auto_save.session = True
 # hosts blocking
 c.content.host_blocking.enabled = True
 c.content.host_blocking.lists = [ 'https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts' ]
+
+# search engines
+c.url.searchengines = {
+    "DEFAULT": "https://duckduckgo.com/?q={}",
+    "google": "https://www.google.com/search?q={}"
+}
