efb3af2532edb28d13a2222056ecb68ec798289f
Damien Tardy-Panis <damien@tardypad.me>
2018-03-12 11:54:34 +0100

    whitelist analytics.google.com in qutebrowser
    adblocked by list
    needed for Firebase dashboards...

--- qutebrowser/config.d/general.py
+++ qutebrowser/config.d/general.py
@@ -4,6 +4,7 @@ c.auto_save.session = True
 # hosts blocking
 c.content.host_blocking.enabled = True
 c.content.host_blocking.lists = [ 'https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts' ]
+c.content.host_blocking.whitelist = [ 'analytics.google.com' ]
 
 # search engines
 c.url.searchengines = {
