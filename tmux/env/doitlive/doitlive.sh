8ba42e4dbe79c647e12f474ab9b508c5f8c6e02f
Damien Tardy-Panis <damien@tardypad.me>
2018-03-18 17:18:40 +0100

    fix tmux doitlive env
    worked before because of the autostarted tmux env
    using the default socket
    it has been replaced recently with the projects socket

--- tmux/env/doitlive/doitlive.sh
+++ tmux/env/doitlive/doitlive.sh
@@ -4,16 +4,27 @@ image_path="${XDG_DATA_HOME:-$HOME/.local/share}/sway/images/doitlive"
 session_name="Fuck it, we'll do it live!"
 window_name="What could possibly go wrong?"
 
+# better do things live on a clean plate
+if tmux -L "${socket}" has-session -t "${session_name}" &> /dev/null; then
+  tmux -L "${socket}" kill-session -t "${session_name}"
+fi
+
+# new FIWDIL session
+tmux -L "${socket}" \
+  new-session -d \
+  -s "${session_name}" \
+  -n "${window_name}"
+
 # define left margin for centered image on left pane
-window_width=$( tmux display -p '#{window_width}' )
+window_width=$( tmux -L "${socket}" display -p '#{window_width}' )
 image_width=$( wc --max-line-length < "${image_path}" )
-margin_left_width=$(( ( (${window_width} / 2) - ${image_width} ) / 2 ))
+margin_left_width=$(( ( (window_width / 2) - image_width ) / 2 ))
 margin_left=$( printf '%*s' ${margin_left_width} )
 
 # define top margin for centered image on left pane
-window_height=$( tmux display -p '#{window_height}' )
+window_height=$( tmux -L "${socket}" display -p '#{window_height}' )
 image_height=$( wc --lines < "${image_path}" )
-margin_top_height=$(( ( ${window_height} - ${image_height} ) / 2 ))
+margin_top_height=$(( ( window_height - image_height ) / 2 ))
 margin_top=$( printf '%*s' ${margin_top_height} | sed 's/ / \n/g' )
 
 # command to display Bill centered until q key is pressed
@@ -21,17 +32,7 @@ display_command="cat <( echo \"${margin_top}\" ) ${image_path} \
   | sed 's/^/${margin_left}/' \
   && read -s -d q"
 
-# better do things live on a clean plate
-if tmux -L "${socket}" has-session -t "${session_name}"; then
-  tmux -L "${socket}" kill-session -t "${session_name}"
-fi
-
-# new FIWDIL session with image on the left and work on the right
-tmux -L "${socket}" \
-  new-session -d \
-  -s "${session_name}" \
-  -n "${window_name}"
-
+# image on the left and work on the right
 tmux -L "${socket}" \
   split-window -h -b \
   -t "${session_name}:${window_name}.1" \
