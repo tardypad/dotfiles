45f58c047ae32c2d28abbb859c94ba272de6f157
Damien Tardy-Panis <damien@tardypad.me>
2018-04-06 18:11:07 +0200

    add some tab moving bindings in qutebrowser and vim
    wanted to use Shift similar to sway moving
    Alt-Shift-1 to move to tab 1
    Alt-Shift-h to move to left tab
    but problem in vim: case insensitive mapping with modifier (or so)
    and Alt-h already used for moving between windows

--- qutebrowser/config.d/bindings.py
+++ qutebrowser/config.d/bindings.py
@@ -24,6 +24,20 @@ config.bind('gt', 'tab-focus', mode='normal')
 config.bind('gT', 'tab-prev', mode='normal')
 config.bind('X', 'tab-close', mode='normal')
 
+# tabs moving
+config.bind('<Space>tm1', 'tab-move 1', mode='normal')
+config.bind('<Space>tm2', 'tab-move 2', mode='normal')
+config.bind('<Space>tm3', 'tab-move 3', mode='normal')
+config.bind('<Space>tm4', 'tab-move 4', mode='normal')
+config.bind('<Space>tm5', 'tab-move 5', mode='normal')
+config.bind('<Space>tm6', 'tab-move 6', mode='normal')
+config.bind('<Space>tm7', 'tab-move 7', mode='normal')
+config.bind('<Space>tm8', 'tab-move 8', mode='normal')
+config.bind('<Space>tm9', 'tab-move 9', mode='normal')
+config.bind('<Space>tm0', 'tab-move 10', mode='normal')
+config.bind('<Space>tmh', 'tab-move -', mode='normal')
+config.bind('<Space>tml', 'tab-move +', mode='normal')
+
 # zooming
 config.unbind('-', mode='normal')
 config.unbind('+', mode='normal')
