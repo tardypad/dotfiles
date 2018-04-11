5b6314440977d51f016cbc61896b51307fafa459
Damien Tardy-Panis <damien@tardypad.me>
2018-04-12 00:41:55 +0200

    add some Arch Linux related search engines in qutebrowser

--- qutebrowser/config.d/general.py
+++ qutebrowser/config.d/general.py
@@ -9,5 +9,8 @@ c.content.host_blocking.whitelist = [ 'analytics.google.com' ]
 # search engines
 c.url.searchengines = {
     "DEFAULT": "https://duckduckgo.com/?q={}",
-    "gg": "https://www.google.com/search?q={}"
+    "gg": "https://www.google.com/search?q={}",
+    "pkg": "https://www.archlinux.org/packages/?q={}",
+    "aur": "https://aur.archlinux.org/packages/?K={}",
+    "wiki": "https://wiki.archlinux.org/index.php?search={}"
 }
