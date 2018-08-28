8d883d36a33701efec69220259c10e6d66d5bcb8
Damien Tardy-Panis <damien@tardypad.me>
2018-08-28 11:42:29 +0200

    add tardypad.me website tmux project env

---
+++ packages/tmux/env/projects/tardypad_me.sh
@@ -0,0 +1,31 @@
+socket="$1"
+
+tardypad_me_path=$( get_project_repo_path tardypad.me )
+
+
+tmux -L "${socket}" \
+  new-session -d \
+  -s 'tardypad_me' \
+  -n 'terminal' \
+  -c "${tardypad_me_path}"
+
+
+tmux -L "${socket}" \
+  new-window \
+  -n 'code' \
+  -c "${tardypad_me_path}" \
+  "vim \
+    -c 'TabooRename TODO' \
+    -c 'tabnew' \
+    -- TODO.md"
+
+
+tmux -L "${socket}" \
+  new-window \
+  -n 'tests' \
+  -c "${tardypad_me_path}"
+
+
+tmux -L "${socket}" \
+  select-window \
+  -t ':terminal'
