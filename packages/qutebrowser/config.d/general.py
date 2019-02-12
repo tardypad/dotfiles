c9c74c6c116b1a254697d59596328385e7188555
Damien Tardy-Panis <damien@tardypad.me>
2019-02-12 15:56:55 +0100

    update qutebrowser fingerprinting minimization values
    using User-Agent of recent version of Chrome on Windows
    to prevent some used websites to mention that my browser is too old
    and limiting the functionalities they offer
    
    using custom Accept header seems to create some issues on some websites Javascript...
    update to corresponding new Chrome version but disable it for now

--- packages/qutebrowser/config.d/general.py
+++ packages/qutebrowser/config.d/general.py
@@ -28,7 +28,7 @@ config.set('content.javascript.enabled', True, '*.local')
 config.set('content.javascript.enabled', True, '*.youtube.com')
 
 # minimize fingerprinting
-c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0"
+c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36"
 c.content.headers.accept_language = "en-US,en;q=0.5"
-c.content.headers.custom = { "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" }
+#c.content.headers.custom = { "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" }
 c.content.headers.do_not_track = False
