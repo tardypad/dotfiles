320198faaa732484fcdd98d7b30a67dc24a4f3ba
Damien Tardy-Panis <damien@tardypad.me>
2019-03-24 18:47:09 +0100

    remove tmux alert message display restriction
    First we need the desktop notification displayed even if the attached
    session and active window is the same, in case we are in a different
    workspace.
    Even if we stay on the same window, the pane where the alert is coming
    from might be hidden from another one being zoomed in, so the message
    displayed internally would still be useful.
    
    Better always displayed both than adding complex logic here.
    The disturbance it can create would be small and quite rare.
    "Worse is better"

--- packages/tmux/scripts/notify_alert.sh
+++ packages/tmux/scripts/notify_alert.sh
@@ -5,20 +5,6 @@ ALERT_SOCKET_PATH="$2"
 ALERT_SESSION_ID="$3"
 ALERT_WINDOW_ID="$4"
 
-CURRENT_SESSION_WINDOW=$(
-  tmux list-sessions \
-    -F '#{?session_attached,attached,} #{session_id}:#{window_id}' \
-    | grep attached \
-    | cut --fields 2 --delimiter ' '
-)
-
-ALERT_SESSION_WINDOW="${ALERT_SESSION_ID}:${ALERT_WINDOW_ID}"
-
-if [[ "${CURRENT_SESSION_WINDOW}" == "${ALERT_SESSION_WINDOW}" ]]; then
-  # do nothing if alert comes from currently attached session and active window
-  exit
-fi
-
 ALERT_SESSION_NAME=$(
   tmux list-sessions -F '#{session_id} #{session_name}' \
     | grep "^${ALERT_SESSION_ID} " \
