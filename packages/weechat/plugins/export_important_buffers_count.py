5ac8ec1d9c5a40cdf67447163f3f60fd97bb5939
Damien Tardy-Panis <damien@tardypad.me>
2019-04-30 11:49:49 +0200

    add weechat plugin to export the count of buffers with important messages
    private buffers and public buffers with highlighted message

---
+++ packages/weechat/plugins/export_important_buffers_count.py
@@ -0,0 +1,59 @@
+SCRIPT_NAME    = 'export_important_buffers_count'
+SCRIPT_AUTHOR  = 'Damien Tardy-Panis <damien@tardypad.me>'
+SCRIPT_VERSION = '0.1.0'
+SCRIPT_LICENSE = 'MIT'
+SCRIPT_DESC    = 'Export the count of buffers with important messages'
+
+IMPORT_OK = True
+
+import os
+
+COUNT_FILE_PATH = os.environ.get('XDG_RUNTIME_DIR') + '/chat_count'
+
+try:
+    import weechat
+except ImportError:
+    print('This script must be run under WeeChat.')
+    IMPORT_OK = False
+
+
+def important_buffers_count():
+    count = 0
+    infolist = weechat.infolist_get('hotlist', '', '')
+    if infolist:
+        while weechat.infolist_next(infolist):
+            priority = weechat.infolist_integer(infolist, 'priority')
+            # only count buffers with highlight or private messages
+            if priority >= 2:
+                count += 1
+        weechat.infolist_free(infolist)
+    return count
+
+
+def write_count(count):
+    try:
+        with open(COUNT_FILE_PATH, 'w') as f:
+            f.write('{}'.format(count))
+            f.close()
+    except (OSError, IOError):
+        return False
+    return True
+
+
+def update_buffers_count(data, signal, signal_data):
+    result = write_count(important_buffers_count())
+
+    if not result:
+        return weechat.WEECHAT_RC_ERROR
+    return weechat.WEECHAT_RC_OK
+
+
+def export_important_buffers_count_main():
+    if not weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION,
+                            SCRIPT_LICENSE, SCRIPT_DESC, '', ''):
+        return
+    weechat.hook_signal('hotlist_changed', 'update_buffers_count', '')
+
+
+if __name__ == '__main__' and IMPORT_OK:
+    export_important_buffers_count_main()
