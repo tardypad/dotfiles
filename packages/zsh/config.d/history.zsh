bdddfed6957831ea04cee2e255aa51bebdde8e3e
Damien Tardy-Panis <damien@tardypad.me>
2019-05-21 16:08:14 +0200

    remove zsh history option that remove unnecessary blanks from commands
    it is useful to remove consecutive spaces but the problem is that it
    also removes newlines. This prevents an easy edition of commands we
    typed on multiple lines

--- packages/zsh/config.d/history.zsh
+++ packages/zsh/config.d/history.zsh
@@ -3,9 +3,6 @@ export HISTFILE="$HOME/.zsh_history"
 export HISTSIZE=10000
 export SAVEHIST=${HISTSIZE}
 
-# remove unnecessary blanks from commands being written to history
-setopt hist_reduce_blanks
-
 # store start and duration of commands
 setopt extended_history
 
