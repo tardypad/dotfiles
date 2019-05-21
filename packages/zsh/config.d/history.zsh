5c097e9d85b2ab82e12703d8f7fb0bbbaba7aad2
Damien Tardy-Panis <damien@tardypad.me>
2019-05-21 15:27:29 +0200

    remove zsh history option that remove duplicate commands
    so that we keep a full history of commands run

--- packages/zsh/config.d/history.zsh
+++ packages/zsh/config.d/history.zsh
@@ -3,10 +3,6 @@ export HISTFILE="$HOME/.zsh_history"
 export HISTSIZE=10000
 export SAVEHIST=${HISTSIZE}
 
-# if a new command line being added to the history list duplicates an older one,
-# the older command is removed from the list
-setopt hist_ignore_all_dups
-
 # remove unnecessary blanks from commands being written to history
 setopt hist_reduce_blanks
 
