1efdffd6bc141f024273f4fbb5485b5c8a7288c0
Damien Tardy-Panis <damien@tardypad.me>
2020-05-15 16:27:45 +0200

    Update "arch packages" tmux env to new "arch system" name

--- files/data/env/tmux/projects/arch_system.sh
+++ files/data/env/tmux/projects/arch_system.sh
@@ -3,18 +3,18 @@
 SOCKET="$1"
 SESSION_NAME="$2"
 
-ARCH_PACKAGES_PATH=$( project-path repo arch-packages )
+ARCH_SYSTEM_PATH=$( project-path repo arch-system )
 
 tmux -L "${SOCKET}" \
   new-session -d \
   -s "${SESSION_NAME}" \
   -n 'terminal' \
-  -c "${ARCH_PACKAGES_PATH}"
+  -c "${ARCH_SYSTEM_PATH}"
 
 tmux -L "${SOCKET}" \
   new-window \
   -n 'code' \
-  -c "${ARCH_PACKAGES_PATH}" \
+  -c "${ARCH_SYSTEM_PATH}" \
   "vim \
     -c 'TabooRename TODO' \
     -c 'tabnew' \
