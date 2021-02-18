0e59d4a47baa8c341c23494237edca997744706f
Damien Tardy-Panis <damien@tardypad.me>
2021-02-18 09:31:39 +0100

    Only keep jp in plott tmux env

--- files/data/env/tmux/projects/plott.sh
+++ files/data/env/tmux/projects/plott.sh
@@ -4,9 +4,7 @@ SOCKET="$1"
 SESSION_NAME="$2"
 
 PLOTT_PATH=$( project-path repo plott )
-FZF_PATH=$( project-path repo fzf )
 JP_PATH=$( project-path repo jp )
-TTYPLOT_PATH=$( project-path repo ttyplot )
 
 tmux -L "${SOCKET}" \
   new-session -d \
@@ -24,32 +22,12 @@ tmux -L "${SOCKET}" \
 
 tmux -L "${SOCKET}" \
   new-window \
-  -n 'fzf' \
-  -c "${FZF_PATH}"
-
-tmux -L "${SOCKET}" \
-  split-window -dh \
-  -c "${FZF_PATH}" \
-  vim
-
-tmux -L "${SOCKET}" \
-  new-window \
   -n 'jp' \
   -c "${JP_PATH}"
 
 tmux -L "${SOCKET}" \
   split-window -dh \
   -c "${JP_PATH}" \
-  vim
-
-tmux -L "${SOCKET}" \
-  new-window \
-  -n 'ttyplot' \
-  -c "${TTYPLOT_PATH}"
-
-tmux -L "${SOCKET}" \
-  split-window -dh \
-  -c "${TTYPLOT_PATH}" \
   vim
 
 tmux -L "${SOCKET}" \
