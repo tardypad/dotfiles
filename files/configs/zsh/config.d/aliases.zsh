34f0776f76f8c238e0d7f1b23a75999d7886627b
Damien Tardy-Panis <damien@tardypad.me>
2021-05-16 10:30:35 +0200

    Remove gron related alias and package from list
    Kept in system packages but no need for special alias and config

--- files/configs/zsh/config.d/aliases.zsh
+++ files/configs/zsh/config.d/aliases.zsh
@@ -106,9 +106,6 @@ alias qalc='noglob qalc'
 # flush all .log files recursively in the current dir
 alias flush-logs="find . -type f -name '*.log' -exec /bin/sh -c \"printf '' > '{}'\" \;"
 
-# simpler ungronning
-alias ungron="gron --ungron"
-
 # Silver searcher default options
 alias ag='ag --color-match="1;{color/search_match/fg}" --color-path="{color/base09/fg}" --color-line-number="{color/base0A/fg}" --case-sensitive --skip-vcs-ignores --follow'
 
