b95f4fd8407faeccb1abe1fd7ac669be4418e707
Damien Tardy-Panis <damien@tardypad.me>
2021-07-07 13:29:56 +0200

    Add g alias for git

--- files/configs/zsh/config.d/aliases.zsh
+++ files/configs/zsh/config.d/aliases.zsh
@@ -29,6 +29,8 @@ alias .a='cat .aliases 2> /dev/null && . ./.aliases'
 
 alias todo='${EDITOR} "${XDG_NOTES_DIR:-$HOME/Notes}/todo.md"'
 
+alias g='git'
+
 alias pj='goto-project repo'
 alias PJ='goto-project main'
 
