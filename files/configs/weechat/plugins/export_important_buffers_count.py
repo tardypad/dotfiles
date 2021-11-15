214cedfd8ea475d10cfd8457cdbc61f89c475115
Damien Tardy-Panis <damien.dev@tardypad.me>
2021-11-15 13:45:38 +0100

    Fix consistency of weechat plugins style
    - double quote usage by default
    - no double empty lines between functions

--- files/configs/weechat/plugins/export_important_buffers_count.py
+++ files/configs/weechat/plugins/export_important_buffers_count.py
@@ -1,44 +1,41 @@
-SCRIPT_NAME    = 'export_important_buffers_count'
-SCRIPT_AUTHOR  = 'Damien Tardy-Panis <damien.dev@tardypad.me>'
-SCRIPT_VERSION = '0.1.0'
-SCRIPT_LICENSE = 'MIT'
-SCRIPT_DESC    = 'Export the count of buffers with important messages'
+SCRIPT_NAME    = "export_important_buffers_count"
+SCRIPT_AUTHOR  = "Damien Tardy-Panis <damien.dev@tardypad.me>"
+SCRIPT_VERSION = "0.1.0"
+SCRIPT_LICENSE = "MIT"
+SCRIPT_DESC    = "Export the count of buffers with important messages"
 
 IMPORT_OK = True
 
 import os
 
-COUNT_FILE_PATH = os.environ.get('XDG_RUNTIME_DIR') + '/chat_count'
+COUNT_FILE_PATH = os.environ.get("XDG_RUNTIME_DIR") + "/chat_count"
 
 try:
     import weechat
 except ImportError:
-    print('This script must be run under WeeChat.')
+    print("This script must be run under WeeChat.")
     IMPORT_OK = False
-
 
 def important_buffers_count():
     count = 0
-    infolist = weechat.infolist_get('hotlist', '', '')
+    infolist = weechat.infolist_get("hotlist", "", "")
     if infolist:
         while weechat.infolist_next(infolist):
-            priority = weechat.infolist_integer(infolist, 'priority')
+            priority = weechat.infolist_integer(infolist, "priority")
             # only count buffers with highlight or private messages
             if priority >= 2:
                 count += 1
         weechat.infolist_free(infolist)
     return count
 
-
 def write_count(count):
     try:
-        with open(COUNT_FILE_PATH, 'w') as f:
-            f.write('{}'.format(count))
+        with open(COUNT_FILE_PATH, "w") as f:
+            f.write("{}".format(count))
             f.close()
     except (OSError, IOError):
         return False
     return True
-
 
 def update_buffers_count(data, signal, signal_data):
     result = write_count(important_buffers_count())
@@ -47,7 +44,6 @@ def update_buffers_count(data, signal, signal_data):
         return weechat.WEECHAT_RC_ERROR
     return weechat.WEECHAT_RC_OK
 
-
 def shutdown_cb():
     result = os.remove(COUNT_FILE_PATH)
 
@@ -55,13 +51,11 @@ def shutdown_cb():
         return weechat.WEECHAT_RC_ERROR
     return weechat.WEECHAT_RC_OK
 
-
 def export_important_buffers_count_main():
     if not weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION,
-                            SCRIPT_LICENSE, SCRIPT_DESC, 'shutdown_cb', ''):
+                            SCRIPT_LICENSE, SCRIPT_DESC, "shutdown_cb", ""):
         return
-    weechat.hook_signal('hotlist_changed', 'update_buffers_count', '')
+    weechat.hook_signal("hotlist_changed", "update_buffers_count", "")
 
-
-if __name__ == '__main__' and IMPORT_OK:
+if __name__ == "__main__" and IMPORT_OK:
     export_important_buffers_count_main()
--- files/configs/weechat/plugins/go_fzf_tmux.py
+++ files/configs/weechat/plugins/go_fzf_tmux.py
@@ -1,9 +1,9 @@
-SCRIPT_NAME    = 'go_fzf_tmux'
-SCRIPT_AUTHOR  = 'Damien Tardy-Panis <damien.dev@tardypad.me>'
-SCRIPT_VERSION = '0.1.0'
-SCRIPT_LICENSE = 'MIT'
-SCRIPT_DESC    = 'Quick jump to buffers using fzf-tmux'
-SCRIPT_COMMAND = 'go_fzf_tmux'
+SCRIPT_NAME    = "go_fzf_tmux"
+SCRIPT_AUTHOR  = "Damien Tardy-Panis <damien.dev@tardypad.me>"
+SCRIPT_VERSION = "0.1.0"
+SCRIPT_LICENSE = "MIT"
+SCRIPT_DESC    = "Quick jump to buffers using fzf-tmux"
+SCRIPT_COMMAND = "go_fzf_tmux"
 
 IMPORT_OK = True
 
@@ -14,60 +14,55 @@ import subprocess
 try:
     import weechat
 except ImportError:
-    print('This script must be run under WeeChat.')
+    print("This script must be run under WeeChat.")
     IMPORT_OK = False
 
 SETTINGS = {
-    'fzf_options': (
-        '-d 20',
-        'Options for fzf command'),
+    "fzf_options": (
+        "-d 20",
+        "Options for fzf command"),
 }
 
 FZF_MAIN_OPTIONS = "--with-nth 2.."
 
 def get_buffers():
     buffers = []
-    infolist = weechat.infolist_get('buffer', '', '')
+    infolist = weechat.infolist_get("buffer", "", "")
     if infolist:
         while weechat.infolist_next(infolist):
-            number = weechat.infolist_integer(infolist, 'number')
-            name = weechat.infolist_string(infolist, 'short_name')
+            number = weechat.infolist_integer(infolist, "number")
+            name = weechat.infolist_string(infolist, "short_name")
             buffers.append({"number": number, "name": name})
         weechat.infolist_free(infolist)
     return buffers
 
-
 def get_buffers_input():
     lines = []
     for buffer in get_buffers():
-        lines.append('{} {}'.format(buffer["number"], buffer["name"]))
+        lines.append("{} {}".format(buffer["number"], buffer["name"]))
     return "\n".join(lines)
 
-
 def process_fzf_result(data, command, rc, out, err):
-    if rc == 0 and out != '':
+    if rc == 0 and out != "":
         switch_to_buffer(out)
     return weechat.WEECHAT_RC_OK
 
-
 def switch_to_buffer(out):
-    selected = out.strip('\n')
-    number = selected.split(' ')[0]
-    weechat.command('', '/buffer *{}'.format(number))
-
+    selected = out.strip("\n")
+    number = selected.split(" ")[0]
+    weechat.command("", "/buffer *{}".format(number))
 
 def run_within_tmux():
     command = 'echo "{}" | fzf-tmux {} {}'.format(
             get_buffers_input(),
-            weechat.config_get_plugin('fzf_options'),
+            weechat.config_get_plugin("fzf_options"),
             FZF_MAIN_OPTIONS)
 
     if not weechat.hook_process_hashtable(
-        'sh', {'arg1': '-c', 'arg2': command},
-        0, 'process_fzf_result', ''):
+        "sh", {"arg1": "-c", "arg2": command},
+        0, "process_fzf_result", ""):
         return weechat.WEECHAT_RC_ERROR
     return weechat.WEECHAT_RC_OK
-
 
 def run_blocking():
     echo_command = 'echo "{}"'.format(get_buffers_input())
@@ -76,36 +71,34 @@ def run_blocking():
                     stdout=subprocess.PIPE)
 
     try:
-        fzf_command = 'fzf {} {}'.format(
-            weechat.config_get_plugin('fzf_options'),
+        fzf_command = "fzf {} {}".format(
+            weechat.config_get_plugin("fzf_options"),
             FZF_MAIN_OPTIONS)
         out = subprocess.check_output(
                 shlex.split(fzf_command),
                 stdin=echo_process.stdout)
-        if out != '':
+        if out != "":
             switch_to_buffer(out)
     except (subprocess.CalledProcessError):
         pass
 
-    weechat.command('', "/window refresh")
+    weechat.command("", "/window refresh")
 
     return weechat.WEECHAT_RC_OK
 
-
 def go_fzf_tmux_cmd(data, buf, args):
-    if os.environ.get('TMUX'):
+    if os.environ.get("TMUX"):
         return run_within_tmux()
     else:
         return run_blocking()
 
-
 def go_fzf_tmux_main():
     if not weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION,
-                            SCRIPT_LICENSE, SCRIPT_DESC, '', ''):
+                            SCRIPT_LICENSE, SCRIPT_DESC, "", ""):
         return
     weechat.hook_command(
         SCRIPT_COMMAND, SCRIPT_DESC,
-        '', '', '', 'go_fzf_tmux_cmd', '')
+        "", "", "", "go_fzf_tmux_cmd", "")
 
     for option, value in SETTINGS.items():
         if not weechat.config_is_set_plugin(option):
@@ -113,6 +106,5 @@ def go_fzf_tmux_main():
             weechat.config_set_desc_plugin(
                 option, '%s (default: "%s")' % (value[1], value[0]))
 
-
-if __name__ == '__main__' and IMPORT_OK:
+if __name__ == "__main__" and IMPORT_OK:
     go_fzf_tmux_main()
