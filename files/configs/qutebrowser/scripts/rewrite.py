d4e3fb12b182e92330a6fde7bbde37839ba5d738
Damien Tardy-Panis <damien.dev@tardypad.me>
2023-08-06 12:11:39 +0200

    Fix YouTube redirection from youtu.be in qutebrowser
    The links don't contain the "/watch?v=" part and only have the video id.

--- files/configs/qutebrowser/scripts/rewrite.py
+++ files/configs/qutebrowser/scripts/rewrite.py
@@ -1,19 +1,28 @@
 from qutebrowser.api import interceptor
 
-def redirect(request: interceptor.Request, host: str):
-    request.request_url.setHost(host)
-    try:
-        request.redirect(request.request_url)
-    except interceptor.interceptors.RedirectException:
-        pass
-
 def rewrite(request: interceptor.Request):
+    rewritten = False
     host = request.request_url.host()
 
     if host in ['twitter.com', 'www.twitter.com', 'mobile.twitter.com']:
-        redirect(request, 'nitter.net');
+        rewritten = True
+        request.request_url.setHost('nitter.net')
 
-    if host in ['youtube.com', 'www.youtube.com', 'youtu.be']:
-        redirect(request, 'yewtu.be');
+    if host in ['youtube.com', 'www.youtube.com']:
+        rewritten = True
+        request.request_url.setHost('yewtu.be')
+
+    if host in ['youtu.be']:
+        rewritten = True
+        video_id = request.request_url.path()
+        request.request_url.setHost('yewtu.be')
+        request.request_url.setPath('/watch')
+        request.request_url.setQuery('v=' + video_id.lstrip('/'))
+
+    if rewritten:
+        try:
+            request.redirect(request.request_url)
+        except interceptor.interceptors.RedirectException:
+            pass
 
 interceptor.register(rewrite)
