af79798fa334ba5d827e061e0e36e5d090dbcc36
Damien Tardy-Panis <damien.dev@tardypad.me>
2023-12-16 20:01:56 +0100

    Replace startup TODO and schedule display with dashboard in scratchpad

---
+++ files/data/env/tmux/general/dashboard.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+SOCKET="$1"
+SESSION_NAME="$2"
+
+tmux -L "${SOCKET}" \
+  new-session -d \
+  -s "${SESSION_NAME}" \
+  -n 'main' \
+  "nvim -c \"let g:goyo_width='95%'\" -c \"let g:goyo_height='95%'\" -c Goyo ${XDG_NOTES_DIR:-$HOME/Notes}/todo.md"
+
+tmux -L "${SOCKET}" \
+  split-window -h \; \
+  select-layout main-vertical \; \
+  send-keys clear Enter schedule Space today Enter
+
+tmux -L "${SOCKET}" \
+  set-option -w -g remain-on-exit on \; \
+  set-option -g status off \; \
+  select-pane -t ':.{left-of}'
+
+# respawn for correct Goyo width and height display
+sleep 0.5;
+tmux -L "${SOCKET}" \
+  respawn-pane -k
