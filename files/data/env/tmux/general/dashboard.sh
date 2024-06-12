2ae689646e88bd7e97eb57fc50d9cfbffa94cc7f
Damien Tardy-Panis <damien.dev@tardypad.me>
2024-06-12 12:47:59 +0200

    Display waiting note first in dashboard
    Then once it is clear that nothing can be acted upon, it should be
    closed, and then the TODO list will be displayed

--- files/data/env/tmux/general/dashboard.sh
+++ files/data/env/tmux/general/dashboard.sh
@@ -7,7 +7,7 @@ tmux -L "${SOCKET}" \
   new-session -d \
   -s "${SESSION_NAME}" \
   -n 'main' \
-  "nvim -c \"let g:goyo_width='95%'\" -c \"let g:goyo_height='95%'\" -c Goyo ${XDG_NOTES_DIR:-$HOME/Notes}/todo.md"
+  "nvim -c \"let g:goyo_width='95%'\" -c \"let g:goyo_height='95%'\" -c Goyo ${XDG_NOTES_DIR:-$HOME/Notes}/waiting.md ${XDG_NOTES_DIR:-$HOME/Notes}/todo.md"
 
 tmux -L "${SOCKET}" \
   split-window -h \; \
