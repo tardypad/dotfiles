c5f048cfc43638d3831ea8df8afcf4bb77e6432e
Damien Tardy-Panis <damien.dev@tardypad.me>
2024-09-19 20:37:29 +0200

    Only display waiting note in dashboard if not empty

--- files/data/env/tmux/general/dashboard.sh
+++ files/data/env/tmux/general/dashboard.sh
@@ -5,11 +5,16 @@ SESSION_NAME="$2"
 
 NOTES_DIR="${XDG_NOTES_DIR:-$HOME/Notes}"
 
+NOTES_FILES="${NOTES_DIR}/todo.md"
+if grep -qvE '# Waiting|^$' "${NOTES_DIR}/waiting.md"; then
+  NOTES_FILES="${NOTES_FILES} ${NOTES_DIR}/waiting.md"
+fi
+
 tmux -L "${SOCKET}" \
   new-session -d \
   -s "${SESSION_NAME}" \
   -n 'main' \
-  "nvim -c \"cd ${NOTES_DIR}\" -c \"let g:goyo_width='95%'\" -c \"let g:goyo_height='95%'\" -c Goyo ${NOTES_DIR}/waiting.md ${NOTES_DIR}/todo.md"
+  "nvim -c \"cd ${NOTES_DIR}\" -c \"let g:goyo_width='95%'\" -c \"let g:goyo_height='95%'\" -c Goyo ${NOTES_FILES}"
 
 tmux -L "${SOCKET}" \
   split-window -h \; \
